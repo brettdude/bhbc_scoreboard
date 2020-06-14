579bad752feb0331970cea10e2268901d62c534c
Brett Clarke <brett@crazy.id.au>
2020-06-14 16:04:13 +1000

    Updated membuf in arduino code for topbot

--- scoreboard1/scoreboard1.ino
+++ scoreboard1/scoreboard1.ino
@@ -116,9 +116,9 @@ void update_scoreboard()
     cmdMessenger.sendCmd(kACK, ScoreVisitorHeading);
     cmdMessenger.sendCmd(kACK, ScoreVisitor);
     
-    // Innings Top or Bottom
-    memset(buf, '\0', 3);
-    cmdMessenger.copyString(buf, 3);
+    // Innigs Top or Bottom
+    memset(buf, '\0', 4);
+    cmdMessenger.copyString(buf, 4);
     strcpy(Topbot,buf);
     cmdMessenger.sendCmd(kACK, TopbotHeading);
     cmdMessenger.sendCmd(kACK, Topbot);
