fd2828f5b2f917fd91b5b9b0996ac9a9a0983cfb
Brett Clarke <brett@crazy.id.au>
2020-06-14 14:00:49 +1000

    Updated sendtoarduino.js to include topbot and multiple ball values

--- scoreboard1/BHBC_Scoreboard-web-interfacev1/js/sendToArduino.js
+++ scoreboard1/BHBC_Scoreboard-web-interfacev1/js/sendToArduino.js
@@ -1,6 +1,6 @@
 		// Simple function to submit the current score values to the PHP script that updates the Arduino
 		function submitForm(){
-			var formData2 = padToSend(ScoreVisitor, 2) + padToSend(Innings, 2) + padToSend(ScoreHome, 2) + padToSend(Balls, 1) + padToSend(Strikes, 1) + padToSend(Outs, 1); 
+			var formData2 = padToSend(ScoreVisitor, 2) + padToSend(Topbot, 1) + padToSend(Innings, 2) + padToSend(ScoreHome, 2) + padToSend(Balls1, 1) + padToSend(Balls2, 1) + padToSend(Strikes, 1) + padToSend(Outs, 1); 
 
             $.ajax({
                 type: "GET",
